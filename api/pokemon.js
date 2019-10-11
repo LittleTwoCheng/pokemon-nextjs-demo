5140fc9b7d8df8a7723125282ef982f2b8ee831c
benchengggv <ben.cheng@gogotech.hk>
2019-10-11 15:54:20 +0800

    initial commit

---
+++ api/pokemon.js
@@ -0,0 +1,44 @@
+import 'isomorphic-fetch'
+
+const BASE_PATH = 'https://pokeapi.co/api/v2'
+const cache = {}
+
+const get = async id => {
+  console.log('🐛 Pokemon API call')
+  if (cache[id])
+    return {
+      data: cache[id],
+    }
+
+  const fetchRes = await fetch(`${BASE_PATH}/pokemon/${id}`)
+  const res = await fetchRes.json()
+
+  cache[id] = res
+  return {
+    data: res,
+  }
+}
+const list = async () => {
+  console.log('🐛 Pokemon API call')
+  const fetchRes = await fetch(`${BASE_PATH}/pokemon`)
+  const res = await fetchRes.json()
+
+  return {
+    data: res.results,
+    count: res.count,
+  }
+}
+
+const captureIdRegex = new RegExp(
+  /https:\/\/pokeapi.co\/api\/v2\/pokemon\/([0-9]+)\//
+)
+export const getId = pokemon => {
+  const { url } = pokemon
+  const match = captureIdRegex.exec(url)
+  if (match.length !== 2) throw Error('unmatch url')
+  return match[1]
+}
+export default {
+  get,
+  list,
+}
